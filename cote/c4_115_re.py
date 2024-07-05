bceae0141094b64aeea6f460fa2c9e324eeeafde
unknown <jvidorca@gmail.com>
2024-07-05 23:51:45 +0900

    Add c4_113, c4_115

---
+++ cote/c4_115_re.py
@@ -0,0 +1,18 @@
+
+i = str(input())
+rownum = int(i[1])
+colnum = int(ord(i[0])) - int(ord('a')) + 1
+
+# 이동할 수 있는 방향
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+# 8가지 방향에 대해 각 위치로 이동이 가능한지 확인
+result = 0
+for step in steps:
+    # 이동하고자 하는 위치 확인
+    next_row = rownum + step[0]
+    next_column = colnum + step[1]
+    # 해당 위치로 이동이 가능하다면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+    
